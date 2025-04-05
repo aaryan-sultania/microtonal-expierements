d106bfcda7825f0dcffc5650a2d3893cc5eee1bd
Aaryan Sultania <98294145+aaryan-sultania@users.noreply.github.com>
2025-04-05 17:27:45 +0000

    Expieriment with changes

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("Hello, Music!")
+import music21
+
+mainscore = music21.stream.Score()
+righthandpart = music21.stream.Part()
+lefthandpart = music21.stream.Part()
+
+ts = music21.meter.TimeSignature('4/4')
+mm = music21.tempo.MetronomeMark(number=120) # 120 bpm
+
+righthandpart.append(ts)
+righthandpart.append(mm)
+lefthandpart.append(ts)
+lefthandpart.append(mm)
+
+mainscore.insert(0, righthandpart)
+mainscore.insert(0, lefthandpart)
+
+mainscore.write("musicxml", "stuff.xml")
