3952523430294d38de9a42928b08f6e59d08ad00
Aaryan Sultania <98294145+aaryan-sultania@users.noreply.github.com>
2025-04-05 18:22:31 +0000

    Create generators

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import music21
 
 mainscore = music21.stream.Score()
+
 righthandpart = music21.stream.Part()
 lefthandpart = music21.stream.Part()
 
@@ -9,8 +10,10 @@ mm = music21.tempo.MetronomeMark(number=120) # 120 bpm
 
 righthandpart.append(ts)
 righthandpart.append(mm)
+
 lefthandpart.append(ts)
 lefthandpart.append(mm)
+
 
 mainscore.insert(0, righthandpart)
 mainscore.insert(0, lefthandpart)
